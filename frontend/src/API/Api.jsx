b4be88da4a655965ec6c1464645cfa1cab4b082c
augustinejoseph <augustine.augustine4@gmail.com>
2023-07-13 16:15:55 +0530

    added  links for frontend

--- frontend/src/API/Api.jsx
+++ frontend/src/API/Api.jsx
@@ -1,3 +1,3 @@
-export const BACKEND_BASE_URL = 'http://localhost:8000/'
+export const BACKEND_BASE_URL = 'https://34.125.185.62/'
 export const ARTICLE_SERVER_NODE_BASE_URL = 'https://node-server-u49f.onrender.com' 
-export const FRONTEND_DOMAIN_NAME = 'http://localhost:5173/'
+export const FRONTEND_DOMAIN_NAME = 'https://datalens-livid.vercel.app/'
