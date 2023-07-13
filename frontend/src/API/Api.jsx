54689511dc039d4ff72659091c47c5a2ce997e05
augustinejoseph <augustine.augustine4@gmail.com>
2023-07-13 12:44:45 +0530

    changed  node server link in react

---
+++ frontend/src/API/Api.jsx
@@ -0,0 +1,3 @@
+export const BACKEND_BASE_URL = 'http://localhost:8000/'
+export const ARTICLE_SERVER_NODE_BASE_URL = 'https://node-server-u49f.onrender.com' 
+export const FRONTEND_DOMAIN_NAME = 'http://localhost:5173/'
