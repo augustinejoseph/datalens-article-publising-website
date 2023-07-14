ada0925a26723274658e551f51b64fb6da4596b9
augustinejoseph <augustine.augustine4@gmail.com>
2023-07-14 08:15:38 +0530

    changed .env domain name from https to http for django server

--- frontend/src/API/Api.jsx
+++ frontend/src/API/Api.jsx
@@ -1,4 +1,4 @@
-export const BACKEND_BASE_URL = 'https://34.125.185.62'
+export const BACKEND_BASE_URL = 'http://34.125.185.62'
 export const ARTICLE_SERVER_NODE_BASE_URL = 'https://node-server-u49f.onrender.com' 
 export const FRONTEND_DOMAIN_NAME = 'https://datalens-livid.vercel.app'
 
